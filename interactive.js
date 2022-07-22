020d23011c09a9baf6b31c8b34c494229337671c
Dali <mohamedalielheni.02@gmail.com>
2022-07-22 07:21:06 +0100

    $git commit -m "done"

---
+++ interactive.js
@@ -0,0 +1,23 @@
+function validateForm(e) {
+    e.preventDefault()
+    var name = document.loginForm.name.value
+    var password = document.loginForm.password.value
+   if (name.length==0)
+    return alert(`name is required`)
+   if (password.length<5)
+    return alert(`password length should more than 5`)
+}
+window.addEventListener('scroll',function(){
+    var anim = document.querySelectorAll('.animation')
+    for(var s=0; s<anim.length;s++){
+        var windowheight= window.innerHeight;
+        var animetop =anim[s].getBoundingClientRect().top;
+        var animepoint = 150;
+        if(animetop<windowheight - animepoint){
+            anim[s].classList.add('active');
+        }
+        else{
+            anim[s].classList.remove('active');
+        }
+    }
+})
